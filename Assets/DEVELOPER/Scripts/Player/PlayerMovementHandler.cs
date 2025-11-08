b84ff4862a34cab092cf2acf4ebb2e7ba98328bf
InoFatih <93407871+fatihhbozkurtt@users.noreply.github.com>
2025-11-08 11:51:29 +0300

    Import: Joystick and AI Navigation packages

---
+++ Assets/DEVELOPER/Scripts/Player/PlayerMovementHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DEVELOPER.Scripts.Player
+{
+    public class PlayerMovementHandler : MonoBehaviour
+    {
+        [Header("Movement Settings")] public float moveSpeed = 5f;
+        public DynamicJoystick joystick;
+
+        private NavMeshAgent agent;
+        private Vector3 moveInput;
+        private Vector3 moveDirection;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+        }
+
+        void Update()
+        {
+            moveInput = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
+
+            if (moveInput.magnitude > 0.1f)
+            {
+                moveDirection = moveInput.normalized * moveSpeed;
+                agent.Move(moveDirection * Time.deltaTime);
+
+                // Yönü joystick yönüne çevir
+                transform.forward = moveInput.normalized;
+            }
+        }
+    }
+}
